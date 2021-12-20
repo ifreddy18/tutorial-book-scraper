5a1ae49035702740190eafee2b8bc01313351538
Freddy Alejandro Michelena Michelena <fmichelena@intelix.ad>
2021-12-20 07:54:48 -0400

    Paso 6 finalizado. Fin del tutorial

--- page.controller.js
+++ page.controller.js
@@ -1,6 +1,6 @@
 // const { pageScraper } = require('./page.scraper');
 const { pageScraper } = require('./page.scraper.tutorial');
-
+const fs = require('fs');
 
 const scrapeAll = async( browserInstace, url ) => {
 
@@ -17,9 +17,21 @@ const scrapeAll = async( browserInstace, url ) => {
 
         // Call the scraper for different set of books to be scraped
         let scrapedData = {};
+
         scrapedData['Travel'] = await pageScraper.scraper(browser, 'Travel');
+        scrapedData['HistoricalFiction'] = await pageScraper.scraper(browser, 'Historical Fiction');
+        scrapedData['Mystery'] = await pageScraper.scraper(browser, 'Mystery');
+        
         await browser.close();
-        console.log(scrapedData)
+        
+        // console.log(scrapedData);
+
+        fs.writeFile("data.json", JSON.stringify(scrapedData), 'utf8', (err) => {
+            if(err) {
+                return console.log(err);
+            }
+            console.log("The data has been scraped and saved successfully! View it at './data.json'");
+        });
 
     } catch ( err ) {
         console.log("Could not resolve the browser instance => ", err);
--- page.scraper.tutorial.js
+++ page.scraper.tutorial.js
@@ -28,17 +28,22 @@ const pageScraper = {
             }, category);
 
             // Navigate to the selected category
+            console.log('Navigating to category', selectedCategory);
             await page.goto(selectedCategory);
 
         }
 
         // Wait for the required DOM to be rendered
         async function scrapeCurrentPage() {
-            await page.waitForSelector('.page_inner');
+            await page.waitForSelector('.page_inner', {
+                timeout: 30000
+            })
+            .catch( err => console.log('.page_inner not found') );
+            
             await page.waitForSelector('.next > a', { 
                 timeout: 10000,
-            });
-            // .catch( console.log );
+            })
+            .catch( err => console.log('Next button no found') );
 
             // Get the link to all the required books
             let urls = await page.$$eval('section ol > li', links => {
@@ -55,29 +60,31 @@ const pageScraper = {
                 return links;
             });
 
-            console.log(urls);
-
-
             // Loop through each of those links, open a new page instance and get the relevant data from them
             let pagePromise = ( link ) => new Promise(async (resolve, reject) => {
                 let dataObj = {};
                 let newPage = await browser.newPage();
+                
                 await newPage.goto(link);
+                
                 dataObj['bookTitle'] = await newPage.$eval('.product_main > h1', text => text.textContent);
-                // dataObj['bookPrice'] = await newPage.$eval('.price_color', text => text.textContent);
-                // dataObj['noAvailable'] = await newPage.$eval('.instock.availability', text => {
-                //     // Strip new line and tab spaces
-                //     text = text.textContent.replace(/(\r\n\t|\n|\r|\t)/gm, "");
-                //     // Get the number of stock available
-                //     let regexp = /^.*\((.*)\).*$/i;
-                //     let stockAvailable = regexp.exec(text)[1].split(' ')[0];
-                //     return stockAvailable;
-                // });
-                // dataObj['imageUrl'] = await newPage.$eval('#product_gallery img', img => img.src);
-                // dataObj['bookDescription'] = await newPage.$eval('#product_description', div => div.nextSibling.nextSibling.textContent);
-                // dataObj['upc'] = await newPage.$eval('.table.table-striped > tbody > tr > td', table => table.textContent);
+                dataObj['bookPrice'] = await newPage.$eval('.price_color', text => text.textContent);
+                dataObj['noAvailable'] = await newPage.$eval('.instock.availability', text => {
+                    // Strip new line and tab spaces
+                    text = text.textContent.replace(/(\r\n\t|\n|\r|\t)/gm, "");
+                    // Get the number of stock available
+                    let regexp = /^.*\((.*)\).*$/i;
+                    let stockAvailable = regexp.exec(text)[1].split(' ')[0];
+                    return stockAvailable;
+                });
+                dataObj['imageUrl'] = await newPage.$eval('#product_gallery img', img => img.src);
+                dataObj['bookDescription'] = await newPage.$eval('#product_description', div => div.nextSibling.nextSibling.textContent);
+                dataObj['upc'] = await newPage.$eval('.table.table-striped > tbody > tr > td', table => table.textContent);
+                
                 resolve(dataObj);
+                
                 await newPage.close();
+            
             });
 
             for (link in urls) {
@@ -86,14 +93,13 @@ const pageScraper = {
                 // console.log(currentPageData);
             }
 
-
-
-            console.log({counter});
+            console.log('Page:', counter );
             counter++;
 
             // When all the data on this page is done, click the next button and start the scraping of the next page
             // You are going to check if this button exist first, so you know if there really is a next page.
             let nextButtonExist = false;
+
             try {
                 const nextButton = await page.$eval('.next > a', a => a.textContent);
                 nextButtonExist = true;
@@ -111,11 +117,8 @@ const pageScraper = {
         
         }
 
-
         let data = await scrapeCurrentPage();
 
-        console.log('========== DATA ==========>');
-        console.log(data);
         return data;
 
     }
